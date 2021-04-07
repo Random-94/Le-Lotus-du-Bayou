760e4d1ccc5167f399f7beb9c76105144651e56e
Lea <lea.brechet@yahoo.fr>
2021-04-07 12:19:11 +0200

    Avancement physique PQ

---
+++ Assets/Scripts/Throw_Paper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw_Paper : MonoBehaviour
+{
+    [SerializeField] private float ThrowForce;
+    //[SerializeField] private GameObject PrefabPaperTest;
+
+    private bool IsThrowing;
+
+    private Rigidbody myRB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var Player = GameObject.FindWithTag("Player");
+        var PlayerScript = Player.GetComponent<Playerbehaviour>();
+
+        var PlayerHand = GameObject.FindWithTag("PlayerHand");
+        var PlayerHandTransform = PlayerHand.GetComponent<Transform>();
+
+        var Camera = GameObject.FindWithTag("MainCamera");
+        var CameraTransform = Camera.GetComponent<Transform>();
+
+        IsThrowing = PlayerScript.isThrowing;
+
+        myRB.AddForce(CameraTransform.forward * ThrowForce);
+
+        /*if(IsThrowing)
+        {
+            myRB.AddForce(PlayerHandTransform.forward * ThrowForce);
+        }*/
+    }
+}
